0e2b029498cb914d826c0e58b2102f5ae12312a5
levshx <pavellevshic@gmail.com>
2021-08-31 00:22:49 +0300

    supar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('MCPROJECT', 'TEST');
 require 'application/lib/Dev.php';
 
 use application\core\Router;
-
+//sosat
 spl_autoload_register(function($class) {
     $path = str_replace('\\', '/', $class.'.php');
     if (file_exists($path)) {
